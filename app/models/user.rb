f0c99253a5669b4e13f04262da7937dfa2885464
guedesvic <victor_hpguedes@hotmail.com>
2022-06-13 21:41:38 -0300

    implemented custom full name method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates_presence_of :first_name, :last_name
+
+  def full_name
+    first_name + " " + last_name
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-    before do
-      @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Victor", last_name: "G")
-    end
+  before do
+    @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Victor", last_name: "G")
+  end
 
+  describe "creation" do
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -14,6 +14,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name methods" do
+    it "has a full name method tha combines first and last name" do
+    expect(@user.full_name).to eq("Victor G")
     end
   end
 end
